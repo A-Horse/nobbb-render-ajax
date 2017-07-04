5d649c1b6d5ef1505f8ddc9037aa8ea1803cdd47
fwchen <crystal.tyan@gmail.com>
2017-07-04 10:45:51 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import R from 'ramda';
 
-export default class NobbbRenderSearch {
-  constructor(configure) {
+export default class NobbbRenderAjax {
+  constructor(meta) {
     this.name = 'ajax';
     this.type = 'render';
 
-    this.configure = configure;
+    this.meta = meta;
   }
 
   getName() {
@@ -18,31 +18,40 @@ export default class NobbbRenderSearch {
     return this.type;
   }
 
-  afterIndexRender(indexData) {
-
-  }
-
   afterArticleRender(rawDocument, articleData, cb) {
     try {
-      fs.writeFile(
-        path.join(articleData.outputDirPath, articleData.fileNameWithoutSuffix + '.json'),
-        JSON.stringify(R.omit('category', articleData)),
-        R.identity
-      )
-    } catch(error) {
+      fs.writeFileSync(
+        path.join(
+          articleData.categoryOutputPath,
+          articleData.articleFileNameWithoutSuffix + '.json'
+        ),
+        JSON.stringify(articleData)
+      );
+    } catch (error) {
       throw error;
     }
   }
 
+  afterwCategoryListRender(data) {
+    fs.writeFileSync(
+      path.join(this.meta.outputRootPath, 'categorys.json'),
+      JSON.stringify(data)
+    );
+  }
+
+  afterwCategoryRender(rendered, data) {
+    fs.writeFileSync(
+      path.join(data.categoryPath, 'index.json'),
+      JSON.stringify(data)
+    );
+  }
+
   afterIndexRender(indexData) {
-    fs.writeFile(
-      path.join(this.configure.outputRoot, 'index.json'),
-      JSON.stringify(indexData)
-    )
+    fs.writeFileSync(
+      path.join(this.meta.outputRootPath, 'index.json'),
+      JSON.stringify(JSON.stringify(indexData))
+    );
   }
 
-  afterRender() {
-  }
-
-
+  afterRender() {}
 }
